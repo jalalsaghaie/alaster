22dfcacea262bfafb8d2c376d5dba35d6633afd7
jalalsaghaie <jalal.sagaie@gmail.com>
2021-11-29 17:49:55 +0330

    feat(app): start (#1)

--- API/Startup.cs
+++ API/Startup.cs
@@ -29,6 +29,10 @@ namespace API
             services.AddControllers();
 
             services.AddApplicationServices(_config);
+
+            services.AddControllers().AddJsonOptions(jsonOptions =>{
+                jsonOptions.JsonSerializerOptions.PropertyNamingPolicy = null;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
